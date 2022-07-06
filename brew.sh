448c3ae8e18fef0e5727192e668715587494a2ec
Ben Meyer-Meisel <ben.meyermeisel@gmail.com>
2022-07-06 22:15:47 +0200

    added packages for brew to install

--- brew.sh
+++ brew.sh
@@ -23,14 +23,14 @@ brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
 # Install a modern version of Bash.
-brew install bash
-brew install bash-completion2
+# brew install bash
+# brew install bash-completion2
 
 # Switch to using brew-installed bash as default shell
-if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
-  chsh -s "${BREW_PREFIX}/bin/bash";
-fi;
+# if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+#   echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+#   chsh -s "${BREW_PREFIX}/bin/bash";
+# fi;
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
@@ -47,55 +47,114 @@ brew install php
 brew install gmp
 
 # Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
+# brew tap bramstein/webfonttools
+# brew install sfnt2woff
+# brew install sfnt2woff-zopfli
+# brew install woff2
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
+# brew install aircrack-ng
+# brew install bfg
+# brew install binutils
+# brew install binwalk
+# brew install cifer
+# brew install dex2jar
+# brew install dns2tcp
+# brew install fcrackzip
+# brew install foremost
+# brew install hashpump
+# brew install hydra
+# brew install john
+# brew install knock
+# brew install netpbm
+# brew install nmap
+# brew install pngcheck
+# brew install socat
+# brew install sqlmap
+# brew install tcpflow
+# brew install tcpreplay
+# brew install tcptrace
+# brew install ucspi-tcp # `tcpserver` etc.
+# brew install xpdf
+# brew install xz
 
 # Install other useful binaries.
 brew install ack
 #brew install exiv2
 brew install git
 brew install git-lfs
-brew install gs
+# brew install gs
 brew install imagemagick --with-webp
-brew install lua
+# brew install lua
 brew install lynx
-brew install p7zip
-brew install pigz
+# brew install p7zip
+# brew install pigz
 brew install pv
 brew install rename
 brew install rlwrap
 brew install ssh-copy-id
 brew install tree
-brew install vbindiff
-brew install zopfli
+# brew install vbindiff
+# brew install zopfli
 
+###################
+# Custom packages #
+###################
+
+echo "Installing brew cask..."
+brew tap homebrew/cask
+
+# Development Languages 
+brew install python3
+
+# (more) Development Tools
+echo "Installing development tools..."
+brew install htop
+brew install curl
+brew install docker
+brew install --cask docker # Docker-Desktop
+brew install --cask github
+brew install --cask iterm2
+brew install --cask visual-studio-code
+
+# Virtualization
+echo "Installing virtualization tools..."
+brew install qemu
+brew install --cask utm
+
+# Web Tools
+echo "Installing web tools..."
+brew install node
+brew install nvm
+brew install --cask firefox
+brew install --cask google-chrome
+
+# File Storage
+echo "Installing file storage tools ..."
+brew install --cask onedrive
+
+# Communication Apps
+echo "Installing communication apps..."
+brew install --cask discord
+brew install --cask microsoft-outlook
+brew install --cask slack
+brew install --cask telegram
+brew install --cask zoom
+
+# General Utility Apps
+echo "Installing general utility apps..."
+brew install --cask bitwarden
+brew install --cask notion
+brew install --cask scroll-reverser
+brew install --cask rectangle
+brew install --cask spotify 
+brew install --cask karabiner-elements
+brew install --cask microsoft-onenote
+echo "Please install if needed:" 
+echo "DisplayLink Manager: https://www.synaptics.com/products/displaylink-graphics/downloads/macos"
+echo "GoodNotes from AppStore"
+echo "Timetable from AppStore"
 # Remove outdated versions from the cellar.
 brew cleanup
+
+echo "Brew install complete!"
