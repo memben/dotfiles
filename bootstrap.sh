698bb54863537d72c97e68d4463f47ce3acbebe5
Ben Meyer-Meisel <ben.meyermeisel@gmail.com>
2022-11-15 12:03:02 +0100

    seperate bootstrap and brew script exec

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,15 +13,13 @@ function doIt() {
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
 
-	# install programs and set shell to bash
-	source ~/brew.sh;
-
 	source ~/.bash_profile;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt;
 else
+	echo "If not yet done, run bash brew.sh";
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
